433ef9022bdbb1ff8cd137ea2da082cd250a688a
Noel Schneider <noelwschneider@gmail.com>
2024-11-18 16:54:20 -0600

    add logic and ui calculator text display
    create variable for tracking display text state in script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function hello() {
-    console.log("Hello, world!");
-};
+let calculatorDisplayText = "";
+
+// html elements
+const calculatorDisplay = document.getElementById("calculator-display");
+const numberBtns = document.querySelectorAll('#calculator #number-pad .number-btn');
+
+numberBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        calculatorDisplayText += btn.value;
+        calculatorDisplay.value = calculatorDisplayText;
+    });
+});
+
