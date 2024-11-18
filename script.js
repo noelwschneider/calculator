78b330068b16ab5c272a5b49c02d859eb09548b2
Noel Schneider <noelwschneider@gmail.com>
2024-11-18 16:57:13 -0600

    refactor ui and logic for handling decimals in number pad
    make decimal-btn id distinct from number-btn class
    
    add validation to script.js
    
    add error and pending style classes for decimal-btn in styles.css

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ let calculatorDisplayText = "";
 // html elements
 const calculatorDisplay = document.getElementById("calculator-display");
 const numberBtns = document.querySelectorAll('#calculator #number-pad .number-btn');
+const decimalBtn = document.getElementById("decimal-btn");
 
 numberBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
         calculatorDisplayText += btn.value;
         calculatorDisplay.value = calculatorDisplayText;
+        decimalBtn.classList.remove('error', 'pending')
     });
 });
 
+decimalBtn.addEventListener('click', () => {
+    if (calculatorDisplayText.endsWith('.')) {
+        calculatorDisplayText = calculatorDisplayText.slice(0, -1);
+        decimalBtn.classList.remove('pending');
+    } else if (calculatorDisplayText.includes('.')) {
+        decimalBtn.classList.add('error');
+    } else {
+        calculatorDisplayText += ".";
+        decimalBtn.classList.add('pending');
+    }
+});
